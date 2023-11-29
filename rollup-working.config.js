d8550a91ef2f69fbb4257508a5d84c8fec43fdf7
David Borges Linhares <dborgesl@cisco.com>
2023-11-29 13:54:17 -0500

    updates to nx 17

--- rollup-working.config.js
+++ rollup-working.config.js
@@ -1,10 +1,11 @@
-const nrwlConfig = require('@nrwl/react/plugins/bundle-rollup');
+const nrwlConfig = require('@nx/react/plugins/bundle-rollup');
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const md5 = require('md5');
 
 module.exports = (config) => {
   const nxConfig = nrwlConfig(config);
+  console.log('NX CONFIG', nxConfig);
   const configWithoutPostCSS = nxConfig.plugins.filter((names) => names.name !== 'postcss');
   return {
       ...nxConfig,
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-const nrwlConfig = require('@nrwl/react/plugins/bundle-rollup');
+const nrwlConfig = require('@nx/react/plugins/bundle-rollup');
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const md5 = require('md5');
@@ -6,38 +6,18 @@ const md5 = require('md5');
 module.exports = (config) => {
   const nxConfig = nrwlConfig(config);
   const configWithoutPostCSS = nxConfig.plugins.filter((names) => names.name !== 'postcss');
-  return [
-    {
-      ...nxConfig,
-      plugins: [
-        postcss({
-          inject: true,
-          extract: true,
-          modules: {
-            generateScopedName: (name, filename, css) => name + '_' + md5(css),
-          },
-          plugins: [autoprefixer],
-        }),
-        ...configWithoutPostCSS,
-      ],
-    },
-    {
-      ...nxConfig,
-      output: {
-        ...nxConfig.output,
-        name: `fat-${nxConfig.output.name}`,
-      },
-      plugins: [
-        postcss({
-          inject: true,
-          extract: false,
-          modules: {
-            generateScopedName: (name, filename, css) => name + '_' + md5(css),
-          },
-          plugins: [autoprefixer],
-        }),
-        ...configWithoutPostCSS,
-      ],
-    },
-  ];
+  return {
+    ...nxConfig,
+    plugins: [
+      postcss({
+        inject: true,
+        extract: true,
+        modules: {
+          generateScopedName: (name, filename, css) => name + '_' + md5(css),
+        },
+        plugins: [autoprefixer],
+      }),
+      ...configWithoutPostCSS,
+    ],
+  };
 };
